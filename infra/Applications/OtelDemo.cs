b9fb4aecf063e1bd9cb29d8303f4c258bf3c9ec9
MartinDotNet <github@my2cents.co.uk>
2023-08-08 20:46:54 +0100

    Refactored to different structure, moved refinery

--- infra/Applications/OtelDemo.cs
+++ infra/Applications/OtelDemo.cs
@@ -6,64 +6,38 @@ using Pulumi.Kubernetes.Types.Inputs.Helm.V3;
 using System.Collections.Generic;
 using Pulumi.Kubernetes.Types.Inputs.Core.V1;
 using Pulumi.Kubernetes.Core.V1;
-using System.IO;
+using Pulumi.Kubernetes.Types.Inputs.Meta.V1;
+
+namespace infra.Applications;
 
 public class OtelDemo : ComponentResource
 {
     public OtelDemo(string name, OtelDemoArgs args, ComponentResourceOptions? options = null)
         : base(name, "aks-otel-demo:chart:otel-demo", options)
     {
-
         var config = new Config();
         var apiKey = config.RequireSecret("honeycombKey");
 
-        var secretApiKey = new Secret("honeycomb-api-key", new SecretArgs
+        var otelDemoNamespace = new Namespace("otel-demo", new NamespaceArgs {
+            Metadata = new ObjectMetaArgs {
+                Name = "otel-demo"
+            }
+        });
+        
+        var secretApiKey = new Secret("honeycomb-api-key-otel-demo", new SecretArgs
         {
+            Metadata = new ObjectMetaArgs {
+                Namespace = otelDemoNamespace.Metadata.Apply(m => m.Name)
+            },
             StringData = {
                 ["honeycomb-api-key"] = apiKey
             }
-        }, new CustomResourceOptions
-        {
-            Provider = args.Provider
         });
 
-        var refineryRulesConfigMap = new ConfigMap("refinery-rules", new ConfigMapArgs {
-            Data = {
-                ["rules.yaml"] = File.ReadAllText("./config-files/refinery/rules.yaml")
-        }}, new CustomResourceOptions
-        {
-            Provider = args.Provider
-        });
-
-        var refinery = new Release("refinery", new ReleaseArgs {
-            Chart = "refinery",
-            Name = "sampling-proxy",
-            RepositoryOpts = new RepositoryOptsArgs {
-                Repo = "https://honeycombio.github.io/helm-charts"
-            },
-            ValueYamlFiles = new FileAsset("./config-files/refinery/values.yaml"),
-            Values = new Dictionary<string, object> {
-                ["environment"] = new [] {
-                    new Dictionary<string, object> {
-                        ["name"] = "REFINERY_HONEYCOMB_API_KEY",
-                        ["valueFrom"] = new Dictionary<string, object> {
-                            ["secretKeyRef"] = new Dictionary<string, object> {
-                                ["name"] = secretApiKey.Id.Apply(a => a.Split("/")[1]),
-                                ["key"] = "honeycomb-api-key"
-                            }
-                        }
-                    }
-                },
-                ["RulesConfigMapName"] = refineryRulesConfigMap.Metadata.Apply(m => m.Name)
-            }
-        }, new CustomResourceOptions
-        {
-            Provider = args.Provider
-        });
-
-        var release = new Release("otel-demo", new ReleaseArgs {
+        var otelDemoRelease = new Release("otel-demo", new ReleaseArgs {
             Chart = "opentelemetry-demo",
             Name = "otel-demo",
+            Namespace = otelDemoNamespace.Metadata.Apply(m => m.Name),
             RepositoryOpts = new RepositoryOptsArgs {
                 Repo = "https://open-telemetry.github.io/opentelemetry-helm-charts"
             },
@@ -84,7 +58,7 @@ public class OtelDemo : ComponentResource
                     ["config"] = new Dictionary<string, object> {
                         ["exporters"] =  new Dictionary<string, object> {
                             ["otlp/honeycomb"] = new Dictionary<string, object> {
-                                ["endpoint"] = refinery.Name.Apply(n => $"http://{n}-refinery:4317"),
+                                ["endpoint"] = Output.Format($"http://{args.RefineryName}:4317"),
                                 ["headers"] = new Dictionary<string, object> {
                                     ["x-honeycomb-team"] = apiKey
                                 },
@@ -96,12 +70,12 @@ public class OtelDemo : ComponentResource
                     }
                 }
             }
-        }, new CustomResourceOptions
-        {
-            Provider = args.Provider
         });
 
         var ingress = new Ingress("otel-demo-frontend", new IngressArgs {
+            Metadata = new ObjectMetaArgs {
+                Namespace = otelDemoNamespace.Metadata.Apply(m => m.Name),
+            },
             Spec = new IngressSpecArgs {
                 IngressClassName = "webapprouting.kubernetes.azure.com",
                 Rules = new IngressRuleArgs {
@@ -125,13 +99,12 @@ public class OtelDemo : ComponentResource
                 }
             }
         }, new CustomResourceOptions {
-            DependsOn = new [] { release },
-            Provider = args.Provider
+            DependsOn = new [] { otelDemoRelease },
         });
     }
 }
 
 public class OtelDemoArgs
 {
-    public ProviderResource Provider { get; set; } = null!;
+    public Input<string> RefineryName { get; set; } = null!;
 }
--- infra/Program.cs
+++ infra/Program.cs
@@ -1,12 +1,18 @@
 ﻿using System.Collections.Generic;
+using Pulumi;
+using infra.Applications;
 
 return await Pulumi.Deployment.RunAsync(() =>
 {
     var cluster = new AKSCluster("aks-otel-demo", new AKSClusterArgs());
+    
+    var refinery = new Refinery("refinery", new RefineryArgs(), 
+        new ComponentResourceOptions { Provider = cluster.Provider });
+
     var otelDemo = new OtelDemo("otel-demo", new OtelDemoArgs{
-        Provider = cluster.Provider
-    });
-    // Export the primary key of the Storage Account
+        RefineryName = refinery.RefineryServiceName
+    }, new ComponentResourceOptions { Provider = cluster.Provider });
+
     return new Dictionary<string, object?>
     {
         ["clusterName"] = cluster.ClusterName,
