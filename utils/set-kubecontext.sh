2b7630a60a8e029d657f66b3cbb628ad37bf9e5e
MartinDotNet <github@my2cents.co.uk>
2023-10-13 16:14:41 +0100

    added static context name to kubecontext script

--- utils/set-kubecontext.sh
+++ utils/set-kubecontext.sh
@@ -3,4 +3,4 @@
 eval "$(pulumi stack output --shell)"
 
 echo "Setting kubectl context to $clusterName in $clusterResourceGroup"
-az aks get-credentials -n $clusterName -g $clusterResourceGroup --overwrite-existing
+az aks get-credentials -n $clusterName -g $clusterResourceGroup --overwrite-existing --context aks-otel-demo
